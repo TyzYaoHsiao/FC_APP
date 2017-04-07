5dcafad94e3440e4c920e0eaf2ae2b0ed856ee21
YanCheng Lin <nick841005@gmail.com>
2017-04-07 12:38:23 +0800

    首頁製作

--- app/src/main/java/com/example/fc_app/MainActivity.java
+++ app/src/main/java/com/example/fc_app/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setInitialScale(1);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.loadUrl("http://www.inskyhotel.com/");
 
         Button map_btn = (Button)findViewById(R.id.map_btn);
 
