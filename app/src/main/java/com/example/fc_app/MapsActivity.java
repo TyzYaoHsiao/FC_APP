1cfd01da9b890abe870cf466f2fef8588dda69a5
TyzYaoHsiao <l85e1o11@gmail.com>
2017-04-21 11:26:09 +0800

    GOOGLE  MAP PolyLine

--- app/src/main/java/com/example/fc_app/MapsActivity.java
+++ app/src/main/java/com/example/fc_app/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.fc_app;
 
+import android.graphics.Color;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +12,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.Polyline;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Polyline mMutablePolyline;
+    private TextView textStatus;
+    private LocationManager lms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +48,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng InSkyHotel = new LatLng(24.182847, 120.644943);
+        LatLng iecs = new LatLng(24.179252, 120.649651);
+        LatLng A = new LatLng(24.182310, 120.644913);
+        LatLng B = new LatLng(24.182269, 120.638913);
+        LatLng C = new LatLng(24.180529, 120.637776);
+        LatLng D = new LatLng(24.179563, 120.637487);
+        LatLng E = new LatLng(24.173840, 120.637428);
+        LatLng F = new LatLng(24.170919, 120.636628);
+        LatLng G = new LatLng(24.173603, 120.633101);
+
+
         mMap.addMarker(new MarkerOptions().position(InSkyHotel).title("InSky Hotel"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(InSkyHotel));
 
 
+        mMap.addMarker(new MarkerOptions().position(iecs).title("逢甲資電館"));
+
+        mMutablePolyline = mMap.addPolyline(new PolylineOptions()
+                .add(InSkyHotel,A,B,C,D,E,F,G)
+                .width(12)
+                .color(Color.BLUE)
+                .clickable(true));
         mMap.getUiSettings().setMapToolbarEnabled(true);
         mMap.animateCamera(CameraUpdateFactory.zoomTo(18)); //放大地圖 18倍
     }
